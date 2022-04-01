ae9ef3920cf338e99ba6a95d4bd0e28c45e94081
Marc Karp <mkarpmar@gmail.com>
2022-04-01 14:23:41 -0700

    Update setup.py to specify SageMaker SDK version
    Using latest SDK version build fails. Specifying working `sagemaker==2.78.0`

--- mlops-template-gitlab/seedcode/mlops-gitlab-project-seedcode-model-build/setup.py
+++ mlops-template-gitlab/seedcode/mlops-gitlab-project-seedcode-model-build/setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as f:
     readme = f.read()
 
 
-required_packages = ["sagemaker"]
+required_packages = ["sagemaker==2.78.0"]
 extras = {
     "test": [
         "black",
