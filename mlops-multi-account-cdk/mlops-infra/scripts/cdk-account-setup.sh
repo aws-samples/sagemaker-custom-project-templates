ef8e855227a895318e1d4973ceb7dcf84060a9b1
Fatema Alkhanaizi <alkhanai@amazon.co.uk>
2022-06-23 15:52:38 +0100

    remove unused import and add more details to the bootstrap script

--- mlops-multi-account-cdk/mlops-infra/scripts/cdk-account-setup.sh
+++ mlops-multi-account-cdk/mlops-infra/scripts/cdk-account-setup.sh
@@ -2,11 +2,11 @@
 # This script setups the aws accounts with the required permissions for CDK deployments, the accounts are bootstrapped
 # and configured to enable cross account access as per the architecture diagram
 
-read -p 'Governance Account: ' gov_account
-read -p 'Dev Account: ' dev_account
-read -p 'PreProd Account: ' preprod_account
-read -p 'Prod Account: ' prod_account
-read -p 'Deployment region: ' region
+read -p 'Governance Account (10-digits): ' gov_account
+read -p 'Dev Account (10-digits): ' dev_account
+read -p 'PreProd Account (10-digits): ' preprod_account
+read -p 'Prod Account (10-digits): ' prod_account
+read -p 'Deployment region (aws regions i.e. us-east-1): ' region
 
 echo 'Updating constants.py file with accounts and region details'
 pattern="[0-9a-zA-Z\-]*"
