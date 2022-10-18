edd8e0903c6537326b77478829131a027cc54ca9
Vishal Naik <vishal49naik49@gmail.com>
2022-10-18 19:36:48 +0000

    backend file updates

--- mlops-tf-pipelines/sagemaker-project-setup/backend.tf
+++ mlops-tf-pipelines/sagemaker-project-setup/backend.tf
@@ -4,11 +4,11 @@
 
 terraform {
   backend "s3" {
-    bucket               = "dev-machine-learning-ops-167084800113" # Based on the default values in the .tfvars files.
+    bucket               = "dev-machine-learning-ops-<acct-id>" # Based on the default values in the .tfvars files.
     key                  = "mlops.tfstate"
     workspace_key_prefix = ""
     region               = "us-west-2"
-    dynamodb_table       = "terraform-tfstate-dev-167084800113" # Based on the default values in the .tfvars files.
+    dynamodb_table       = "terraform-tfstate-dev-<acct-id>" # Based on the default values in the .tfvars files.
     encrypt              = true
   }
 }
