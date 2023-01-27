12c22ffe2441b2c8bf7fee9181ca3bff68c25520
Clay Elmore <celmore25@gmail.com>
2023-01-27 00:53:51 +0000

    shadow deployment additions

--- mlops-shadow-deployment/seedcode/deploy/build.py
+++ mlops-shadow-deployment/seedcode/deploy/build.py
@@ -52,14 +52,28 @@ def get_approved_package(model_package_group_name):
         # Return the pmodel package arn
         model_package_arn = approved_packages[0]["ModelPackageArn"]
         logger.info(f"Identified the latest approved model package: {model_package_arn}")
-        return model_package_arn
+        
+        # find the current prod model to use for shadow testing
+        project_name = 'shadow'
+        prod_endpoint_name = f'{project_name}-prod'
+        if prod_endpoint_name in [i['EndpointName'] for i in sm_client.list_endpoints()['Endpoints']]:
+            logger.info('production endpoint exists')
+            conf = sm_client.describe_endpoint(EndpointName=f'{project_name}-prod')['EndpointConfigName']
+            model = sm_client.describe_endpoint_config(EndpointConfigName=conf)['ProductionVariants'][0]['ModelName']
+            current_prod_model = sm_client.describe_model(ModelName=model)['Containers'][0]['ModelPackageName']
+        else:
+            current_prod_model = model_package_arn
+            logger.info('production endpoint not in existence')
+        logger.info(f'Using {current_prod_model} as current prod model')
+        
+        return model_package_arn, current_prod_model
     except ClientError as e:
         error_message = e.response["Error"]["Message"]
         logger.error(error_message)
         raise Exception(error_message)
 
 
-def extend_config(args, model_package_arn, stage_config):
+def extend_config(args, model_package_arn, current_prod_model, stage_config):
     """
     Extend the stage configuration with additional parameters and tags based.
     """
@@ -72,6 +86,7 @@ def extend_config(args, model_package_arn, stage_config):
     new_params = {
         "SageMakerProjectName": args.sagemaker_project_name,
         "ModelPackageName": model_package_arn,
+        "CurrentProdModelPackageName": current_prod_model,
         "ModelExecutionRoleArn": args.model_execution_role,
         "DataCaptureUploadPath": "s3://" + args.s3_bucket + '/datacapture-' + stage_config["Parameters"]["StageName"],
     }
@@ -91,7 +106,7 @@ def extend_config(args, model_package_arn, stage_config):
 def get_pipeline_custom_tags(args, sm_client, new_tags):
     try:
         response = sm_client.list_tags(
-                ResourceArn=args.sagemaker_project_arn)
+                ResourceArn=args.sagemaker_project_arn.lower())
         project_tags = response["Tags"]
         for project_tag in project_tags:
             new_tags[project_tag["Key"]] = project_tag["Value"]
@@ -133,14 +148,23 @@ if __name__ == "__main__":
     parser.add_argument("--sagemaker-project-name", type=str, required=True)
     parser.add_argument("--sagemaker-project-arn", type=str, required=False)
     parser.add_argument("--s3-bucket", type=str, required=True)
+    
     parser.add_argument("--import-staging-config", type=str, default="staging-config.json")
+    parser.add_argument("--import-shadow-config", type=str, default="shadow-config.json")
     parser.add_argument("--import-prod-config", type=str, default="prod-config.json")
+    
     parser.add_argument("--export-staging-config", type=str, default="staging-config-export.json")
     parser.add_argument("--export-staging-params", type=str, default="staging-params-export.json")
     parser.add_argument("--export-staging-tags", type=str, default="staging-tags-export.json")
+    
+    parser.add_argument("--export-shadow-config", type=str, default="shadow-config-export.json")
+    parser.add_argument("--export-shadow-params", type=str, default="shadow-params-export.json")
+    parser.add_argument("--export-shadow-tags", type=str, default="shadow-tags-export.json")
+    
     parser.add_argument("--export-prod-config", type=str, default="prod-config-export.json")
     parser.add_argument("--export-prod-params", type=str, default="prod-params-export.json")
     parser.add_argument("--export-prod-tags", type=str, default="prod-tags-export.json")
+    
     parser.add_argument("--export-cfn-params-tags", type=bool, default=False)
     args, _ = parser.parse_known_args()
 
@@ -149,22 +173,32 @@ if __name__ == "__main__":
     logging.basicConfig(format=log_format, level=args.log_level)
 
     # Get the latest approved package
-    model_package_arn = get_approved_package(args.model_package_group_name)
+    model_package_arn, current_prod_model = get_approved_package(args.model_package_group_name)
 
     # Write the staging config
     with open(args.import_staging_config, "r") as f:
-        staging_config = extend_config(args, model_package_arn, json.load(f))
+        staging_config = extend_config(args, model_package_arn, current_prod_model, json.load(f))
     logger.debug("Staging config: {}".format(json.dumps(staging_config, indent=4)))
     with open(args.export_staging_config, "w") as f:
         json.dump(staging_config, f, indent=4)
     if (args.export_cfn_params_tags):
         create_cfn_params_tags_file(staging_config, args.export_staging_params, args.export_staging_tags)
+      
+    # Write the shadow config
+    with open(args.import_shadow_config, "r") as f:
+        shadow_config = extend_config(args, model_package_arn, current_prod_model, json.load(f))
+    logger.debug("Shadow config: {}".format(json.dumps(shadow_config, indent=4)))
+    with open(args.export_shadow_config, "w") as f:
+        json.dump(shadow_config, f, indent=4)
+    if (args.export_cfn_params_tags):
+        create_cfn_params_tags_file(shadow_config, args.export_shadow_params, args.export_shadow_tags)
 
     # Write the prod config for code pipeline
     with open(args.import_prod_config, "r") as f:
-        prod_config = extend_config(args, model_package_arn, json.load(f))
+        prod_config = extend_config(args, model_package_arn, current_prod_model, json.load(f))
     logger.debug("Prod config: {}".format(json.dumps(prod_config, indent=4)))
     with open(args.export_prod_config, "w") as f:
         json.dump(prod_config, f, indent=4)
     if (args.export_cfn_params_tags):
         create_cfn_params_tags_file(prod_config, args.export_prod_params, args.export_prod_tags)
+
