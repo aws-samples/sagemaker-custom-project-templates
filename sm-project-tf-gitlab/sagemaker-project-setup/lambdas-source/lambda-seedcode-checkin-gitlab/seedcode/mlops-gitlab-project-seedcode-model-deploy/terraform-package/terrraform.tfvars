81da1dc0ae44514c0276873bce581d468d76d449
Vishal Naik <vsnak@amazon.com>
2023-04-12 18:53:11 +0000

    adding terraform package for endpoint

---
+++ sm-project-tf-gitlab/sagemaker-project-setup/lambdas-source/lambda-seedcode-checkin-gitlab/seedcode/mlops-gitlab-project-seedcode-model-deploy/terraform-package/terrraform.tfvars
@@ -0,0 +1,4 @@
+# Region
+stage_name              = "staging"
+endpoint_instance_count = "1"
+endpoint_instance_type  = "ml.m5.large"
---
+++ sm-project-tf-gitlab/sagemaker-project-setup/lambdas-source/lambda-seedcode-checkin-gitlab/seedcode/mlops-gitlab-project-seedcode-model-deploy/terraform-package/variables.tf
@@ -0,0 +1,36 @@
+# Variables
+#------------
+variable "sagemaker_project_name" {
+  description = "AWS region identifier"
+}
+
+variable "sagemaker_project_id" {
+  description = "AWS region identifier"
+}
+
+variable "model_exec_role_arn" {
+  description = "AWS region identifier"
+}
+
+
+variable "model_registry_artifact" {
+  description = "AWS region identifier"
+}
+
+variable "model_inference_image" {
+  description = "AWS region identifier"
+}
+
+variable "endpoint_instance_count" {
+  description = "AWS region identifier"
+}
+
+
+variable "endpoint_instance_type" {
+  description = "AWS region identifier"
+}
+
+
+variable "endpoint_stage_name" {
+  description = "AWS region identifier"
+}
